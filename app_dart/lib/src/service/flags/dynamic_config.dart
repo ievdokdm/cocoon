7252f3c6d75fefdb8834097ae0efde43c396c31b
Dmitry Grand <dmgr@google.com>
2025-12-09 13:33:17 -0800

    draft implementation of config and data layer

--- app_dart/lib/src/service/flags/dynamic_config.dart
+++ app_dart/lib/src/service/flags/dynamic_config.dart
@@ -158,6 +158,8 @@ extension _YamlMapToMap on YamlMap {
         '$key': value.asMap
       else if (value is YamlList)
         '$key': value.asList
+      else if (value is YamlScalar)
+        '$key': value.value
       else
         '$key': value,
   };
@@ -165,12 +167,14 @@ extension _YamlMapToMap on YamlMap {
 
 extension _YamlListToList on YamlList {
   List<Object?> get asList => <Object?>[
-    for (final value in nodes)
-      if (value is YamlMap)
-        value.asMap
-      else if (value is YamlList)
-        value.asList
+    for (final node in nodes)
+      if (node is YamlMap)
+        node.asMap
+      else if (node is YamlList)
+        node.asList
+      else if (node is YamlScalar)
+        node.value
       else
-        value,
+        node,
   ];
 }
